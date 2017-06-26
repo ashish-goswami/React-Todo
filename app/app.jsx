d93888afd6193450e6cffe3ddafb79a72e804556
Ashish <ashish@Ashishs-MacBook-Pro.local>
2017-06-26 09:13:12 +0530

    Initial Commit

---
+++ app/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hasHistory} = require('react-router');
+
+var TodoApp = require('TodoApp');
+
+// load foundation
+// require('style!css!foundation-sites/dist/css/foundation.min.css')
+$(document).foundation();
+
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <TodoApp/>,
+  document.getElementById('app')
+);
